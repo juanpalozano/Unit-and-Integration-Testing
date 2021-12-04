3198c5cd0eb69b79101130a8ded6b14aa5ac1b32
juanpalozano <jlozano@urbanodigital.com>
2021-12-04 09:50:46 -0500

    Component elements test

--- src/tests/GreetingComponent.test.js
+++ src/tests/GreetingComponent.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
 import GreetingComponent from '../GreetingComponent';
+//import { render } from '@testing-library/react';
+
 
 describe('Test File: GrettingComponent.js', () => {
+    /*
+    Jest Version
     test('Must show message: "Hello Developer!"', () =>{
         const greetingTitle = 'Hello Developer!';
         const { getByText } = render(<GreetingComponent greetingTitle={greetingTitle} />);
@@ -12,5 +17,26 @@ describe('Test File: GrettingComponent.js', () => {
         const greetingSubtitle = '¿Are you ready to code today?';
         const { getByText } = render(<GreetingComponent greetingSubtitle={greetingSubtitle} />);
         expect(getByText(greetingSubtitle)).toBeInTheDocument();
+    });
+    */
+    test('The component should display the title "Hello Developer!".', () => {
+        const greetingTitle = 'Hello Developer!';
+        const wrapper = shallow(<GreetingComponent greetingTitle={greetingTitle} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+    test('The component should display the subtitle "¿Are you ready to code today?".', () => {
+        const greetingSubtitle = '¿Are you ready to code today?';
+        const wrapper = shallow(<GreetingComponent greetingSubtitle={greetingSubtitle} />);
+        expect(wrapper).toMatchSnapshot();
+    });
+    test('The component should display the subtitle send from props.', () => {
+        const greetingSubtitle = 'This is a test subtitle.';
+        const wrapper = shallow(
+            <GreetingComponent
+                greetingSubtitle={greetingSubtitle}
+            />
+        );
+        const subtitleText = wrapper.find('p').text();
+        expect(subtitleText).toBe(greetingSubtitle);
     });
 });
